e7b8d5566d9000c775f799fd07d65bcb25520920
Matt Jewell <mattisfrommars@mattbook-pro.lan>
2021-05-18 20:37:27 +0200

    initial commit

---
+++ src/main/kotlin/dnaComplement/Solution.kt
@@ -0,0 +1,10 @@
+package dnaComplement
+
+val complement = mapOf("A" to "T", "T" to "A", "C" to "G", "G" to "C")
+
+
+fun makeComplement(dna: String): String {
+    return dna.map {
+        complement.getOrDefault(it.toString(), "")
+    }.joinToString("")
+}
